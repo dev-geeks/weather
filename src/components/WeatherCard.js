7c894d5a1ca98a6360519bd33d250b31453aad11
dev-geeks <gurpreets1597@gmail.com>
2023-04-04 21:29:44 +0530

    Made Weather Card Dynamic using API data (completed)

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const WeatherCard = ({ tempInfo }) => {
+    const [weather, setWeather] = useState("");
     const {
         temp,
         humidity,
@@ -11,19 +12,49 @@ const WeatherCard = ({ tempInfo }) => {
         country,
         sunset,
     } = tempInfo;
+
+    let sec = sunset;
+    let date = new Date(sec *1000);
+    let time = `${date.getHours()}: ${date.getMinutes() }`
+
+    useEffect(()=>{
+        if(weatherMood){
+            switch (weatherMood){
+                case "Clouds" : 
+                setWeather('wi-day-cloudy');
+                break;
+
+                case "Haze" :
+                setWeather("wi-fog");
+                break;
+
+                case "Clear":
+                setWeather("wi-day-sunny");
+                break;
+
+                case "Mist":
+                setWeather("wi-dust");
+                break;
+
+                default:
+                setWeather("wi-day-sunny");
+                break;
+            }
+        }
+    },[weatherMood])
     return(
         <div>
             <article className="widget">
                 <div className="weatherIcon">
-                    <i className={"wi wi-day-sunny"}></i>
+                    <i className={`wi ${weather}`}></i>
                 </div>
                 <div className="weatherInfo">
                     <div className="temperature">
-                        <span>25&deg;</span>
+                        <span>{temp}&deg;</span>
                     </div>    
                     <div className="description">
-                        <div className="weatherCondition">SUnney</div>
-                        <div className="place">Pune, India</div>
+                        <div className="weatherCondition">{weatherMood}</div>
+                        <div className="place">{name}, {country}</div>
                     </div>  
                 </div>
                 <div className="date" >{ new Date().toLocaleString()}</div>
@@ -31,21 +62,21 @@ const WeatherCard = ({ tempInfo }) => {
                     <div className="temp-info-minmax">
                         <div className="two-sided-section">
                             <p><i className={"wi wi-sunset"}></i></p>
-                            <p className="extra-info-leftside"> 19.19 <br/> Sunset</p>
+                            <p className="extra-info-leftside">{time} PM <br/> Sunset</p>
                         </div>
                         <div className="two-sided-section">
                             <p><i className={"wi wi-humidity"}></i></p>
-                            <p className="extra-info-leftside"> 19.19 <br/> Humidity</p>
+                            <p className="extra-info-leftside">{humidity}<br/> Humidity</p>
                         </div>
                     </div>
                     <div className="weather-extra-info">
                     <div className="two-sided-section">
                             <p><i className={"wi wi-rain"}></i></p>
-                            <p className="extra-info-leftside"> 19.19 <br/> Pressure</p>
+                            <p className="extra-info-leftside"> {pressure} <br/> Pressure</p>
                         </div>
                         <div className="two-sided-section">
                             <p><i className={"wi wi-strong-wind"}></i></p>
-                            <p className="extra-info-leftside"> 19.19 <br/> Speed</p>
+                            <p className="extra-info-leftside"> {speed} <br/> Speed</p>
                         </div>
                     </div>
                 </div>
