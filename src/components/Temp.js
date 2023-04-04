0df1ba11203e29b22db7047f0fc932aa7f0f37f3
dev-geeks <gurpreets1597@gmail.com>
2023-04-04 20:56:18 +0530

    API setup

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -4,14 +4,42 @@ import "./Temp.css";
 const Temp = () =>{
 
     const [search, setSearch] = useState("mohali");
+    cosnt [tempInfo, setTempinfo] = useState({});
+    const getWeatherInfo = async () =>{
+        try{
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=40c3345589e63b8a91dce2a934daf9c5`;
 
-    const getWeatherInfo = () =>{
+            const res = await fetch(url);
+            const data = await res.json();
 
+            const {temp, humidity, pressure} = data.main;
+            const {main: weatherMood } = data.weather[0];
+            const {name} = data;
+            const {speed} = data.wind;
+            const { country, sunset } = data.sys;
+
+            const NewWeatherInfo = {
+                temp,
+                humidity,
+                pressure,
+                weatherMood,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+            setTempinfo(NewWeatherInfo);
+            console.log(temp);
+            console.log(data);
+        }
+        catch(error){
+            console.log(error);
+        }
     }
 
     useEffect(()=>{
         getWeatherInfo();
-    })
+    },[])
     return(
         <div>
             <div className="wrap">
